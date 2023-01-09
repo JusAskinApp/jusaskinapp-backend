ba790cb09263dff9bb4f1ce986f471ff00f7c846
haseeb4091 <jasialnew1@gmail.com>
2023-01-09 16:28:12 +0500

    Signup endpoint

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const firebase = require("firebase/app");
 const firestore = require("firebase/firestore");
-
+const admin = require("firebase-admin");
 const cors = require("cors");
 const express = require("express");
 
-require("dotenv").config()
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -23,59 +23,62 @@ const router = express.Router();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.API_KEY ,
+  apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROCESS_ID,
+  projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MSG_ID,
-  appId: process.env.APP_ID ,
+  appId: process.env.APP_ID,
   measurementId: process.env.MEASURE_ID,
 };
-  
+
 // Initialize Firebase
-const app2 = firebase.initializeApp(firebaseConfig);
+// const app2 = firebase.initializeApp(firebaseConfig);
+admin.initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
+const db = admin.firestore();
 
-const db = firestore.getFirestore(app2);
-const ref = firestore.collection(db, "users");
-
+router.get("/getUsers", (req, res) => {
+  let users = [];
+  // Reference the 'users' collection
+  db.collection("users")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+        users.push(doc.data());
+      });
+      res.status(200).send(users);
+    })
+    .catch((err) => {
+      console.log("Error getting documents", err);
+    });
+});
 router.get("/", (req, res) => {
   res.json(db);
 });
 
-router.get("/getUsers", (req, res) => {
-  getDocs(ref).then((querySnapshot) => {
-    res.status(200).send(querySnapshot);
-  });
+router.post("/signup", function (req, res) {
+  console.log(req.body);
+  db.collection("users")
+    .add({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      type: req.body.type,
+    })
+    .then((data) => {
+      console.log(data.id);
+      res.send(`user added ${JSON.stringify(data)}`);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(err);
+    });
 });
-
-// router.post("/upload", function (req, res) {
-//   let sampleFile;
-//   // let uploadPath;
-
-//   firestore
-//     .addDoc(ref, {
-//       name: req.body.name,
-//       base64: "asdad".toString(),
-//       type: req.body.type,
-//       author: req.body.author,
-//       translator: req.body.translator,
-//       rating: req.body.rating,
-//       year: req.body.year,
-//       pages: req.body.pages,
-//       description: req.body.description,
-//     })
-//     .then((data) => {
-//       console.log(data.id);
-//       res.send(`File uploaded! ${JSON.stringify(data)}`);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).send(err);
-//     });
-// });
-
+app.use(express.json());             // for application/json
+app.use(express.urlencoded());
 app.use(router);
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT)
-})
+  console.log("Example app listening on port " + PORT);
+});
