b704a2eb4fe0da2c1d982204b608adf11f63d48c
haseeb4091 <jasialnew1@gmail.com>
2023-01-10 15:15:50 +0500

    login done sign up changes

--- routes/users.js
+++ routes/users.js
@@ -3,43 +3,51 @@ const admin = require("firebase-admin");
 const router = express.Router();
 const app = express();
 const db = admin.firestore();
-router.use(express.json());          // for application/json
+router.use(express.json()); // for application/json
 router.use(express.urlencoded());
-router.get("/", (req, res) => {
-  let users = [];
+router.post("/login", (req, res) => {
   db.collection("users")
+    .where("email", "==", req.body.email)
+    .where("password", "==", req.body.password)
     .get()
     .then((snapshot) => {
-      snapshot.forEach((doc) => {
-        console.log(doc.id, "=>", doc.data());
-        users.push(doc.data());
-      });
-      res.status(200).send(users);
-    })
-    .catch((err) => {
-      console.log("Error getting documents", err);
+      if (snapshot.empty) {
+        console.log("Not Found!");
+        res.status(404);
+      } else {
+        console.log("Name and email match!");
+        const docData = snapshot.docs[0].data();
+        res.status(200).send(docData);
+      }
     });
 });
 
 // POST route to add a new user
-router.post("/", (req, res) => {
-  console.log(req.body);
+router.post("/signup", (req, res) => {
   db.collection("users")
-    .add({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      type: req.body.type,
-    })
-    .then((data) => {
-      console.log(data.id);
-      res.send(`user added ${JSON.stringify(data)}`);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send(err);
+    .where("email", "==", req.body.email)
+    .get()
+    .then((snapshot) => {
+      if (snapshot.empty) {
+        db.collection("users")
+          .add({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            type: req.body.type,
+          })
+          .then((data) => {
+            console.log(data.id);
+            res.send(`user added ${JSON.stringify(data)}`);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).send(err);
+          });
+      } else {
+        res.status(400).send("Email already exists");
+      }
     });
 });
-
 
 module.exports = router;
