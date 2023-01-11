ea4d2ca847e5968940d775cc87824df444b64b61
haseeb4091 <jasialnew1@gmail.com>
2023-01-11 12:41:05 +0500

    professiondetails and student details

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,33 @@ const admin = require("firebase-admin");
 const router = express.Router();
 const app = express();
 const db = admin.firestore();
+var professionalDetailsObject = {
+  ID: "",
+  Name: "",
+  DoB: "",
+  Rating: "",
+  Description: "",
+  Qualifications: "",
+  Experience: "",
+  AnswersIDs: [],
+  ResourcesIDs: [],
+  SchedulingIDs: [],
+  Pricing: "",
+};
+const studentDetailsObject = {
+  ID: "",
+  Name: "",
+  DoB: "",
+  AreasOfInterest: [],
+  GroupsIDs: [],
+  ResourcesIDs: [],
+  ConnectionsIDs: [],
+  Points: 0,
+  BadgesIDs: [],
+  CoursesIDs: [],
+  BlogPostIDs: [],
+  PodcastIDs: [],
+};
 router.use(express.json()); // for application/json
 router.use(express.urlencoded());
 router.post("/login", (req, res) => {
@@ -29,8 +56,7 @@ router.post("/signup", (req, res) => {
     .get()
     .then((snapshot) => {
       if (snapshot.empty) {
-        db.collection("users")
-          .add({
+        db.collection("users").add({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password,
@@ -38,7 +64,21 @@ router.post("/signup", (req, res) => {
           })
           .then((data) => {
             console.log(data.id);
-            res.send(`user added ${JSON.stringify(data)}`);
+            if (req.body.type === "Profesional") {
+              professionalDetailsObject.ID = data.id;
+              professionalDetailsObject.Name = req.body.name;
+              db.collection("professionalDetails")
+                .doc(data.id)
+                .set(professionalDetailsObject);
+              res.send(`user added success`);
+            } else if (req.body.type === "Student") {
+              studentDetailsObject.ID = data.id;
+              studentDetailsObject.Name = req.body.name;
+              db.collection("studentDetails")
+                .doc(data.id)
+                .set(studentDetailsObject);
+              res.send(`user added success`);
+            }
           })
           .catch((err) => {
             console.log(err);
