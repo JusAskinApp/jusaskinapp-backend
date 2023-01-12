32191154dcbb80c6999481490847d7c2deef5e3c
NasirAziz <nasiraziz08@gmail.com>
2023-01-12 11:34:33 +0500

    added some blogposts endpoints

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const admin = require("./connection/firebase");
 const cors = require("cors");
 const express = require("express");
 require("dotenv").config();
+const blogRoutes = require("./routes/blogPosts.jsx");
+
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
   origin: "*",
@@ -11,6 +13,7 @@ const corsOptions = {
 const app = express();
 app.use(cors(corsOptions));
 app.use("/users", require("./routes/users"));
+app.use("/api/blogPosts", blogRoutes);
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
